9618da2aaef6ffe53d7b2f49e007be63ceff224a
jimpatton <jimpatton5150@gmail.com>
2025-01-26 18:42:31 -0500

    added chapter 4

--- Guitar-shop-EXERCISES-Homework.sql
+++ Guitar-shop-EXERCISES-Homework.sql
@@ -37,3 +37,50 @@ SELECT OrderID, OrderDate, ShipDate
 
 --EXERCISE 7 - return Price, TaxRate, TaxAmount, Total  - without FROM clause
 SELECT 100 AS Price, .07 as TaxRate, (100 * .07) AS TaxAmount, (100 + (100 * .07)) as Total
+
+
+--CHAPTER 4
+-- EXERCISE ! - return CategoryName, ProductName, ListPrice - from Categories and Products tables - order by category, product ascending order
+SELECT CategoryName, ProductName, ListPrice
+  FROM Categories C
+  JOIN Products P ON C.CategoryID = P.CategoryID
+  ORDER BY CategoryName, ProductName;
+
+--EXERCISE 2 - return FirstName, LastName, Line1, City, State, ZipCode - from Customers, Addresses - for allan.sherwood@yahoo.com
+SELECT FirstName, LastName, Line1, City, State, ZipCode
+  FROM Customers C
+  JOIN Addresses A ON C.CustomerID = A.CustomerID
+  WHERE EmailAddress = 'allan.sherwood@yahoo.com';
+
+--EXERCISE 3 - return FirstName, LastName, Line1, City, State, ZipCode - from Customers, Addresses - only shipping addresses
+SELECT FirstName, LastName, Line1, City, State, ZipCode
+  FROM Customers C
+  JOIN Addresses A ON C.CustomerID = A.CustomerID
+  Where c.ShippingAddressID < c.BillingAddressID; - WRONG
+
+SELECT *
+  From Customers
+
+SELECT *
+  FROM Addresses
+
+--EXERCISE 4 - return LastName, FirstName, OrderDate, ProductName, ItemPrice, DiscountAmount, Quantity - from Customers, orders, orderitems, product - sort Lastname, oderdate, productname 
+SELECT LastName, FirstName, OrderDate, ProductName, ItemPrice, DiscountAmount, Quantity
+  FROM Customers C
+  JOIN Orders O ON C.CustomerID = O.CustomerID
+  JOIN OrderItems OI ON O.OrderID = OI.OrderID
+  JOIN Products P ON OI.ProductID = P.ProductID
+  ORDER BY LastName, OrderDate, ProductName;
+
+--EXERCISE 5 - return ProductName, ListPrice - from Products - one row for each product that has same price as another - sort by product name
+SELECT p1.ProductName, p1.ListPrice
+  FROM Products P1
+  JOIN Products P2 ON P1.ListPrice = P2.ListPrice
+  Order BY ProductName; - WRONG
+
+--EXERCISE 6 - return CategoryName, ProductID - from Catagories, Products - 1 row for each category not used
+SELECT CategoryName, ProductID
+  FROM Categories C
+  LEFT JOIN Products P ON C.CategoryID = P.CategoryID
+  Where ProductID IS NULL;
+
